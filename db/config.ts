0e55031ddc556960f3a8a07deb63891bb67e021e
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-07 16:38:19 +0200

    implemented course and students-list in index.astro

--- db/config.ts
+++ db/config.ts
@@ -1,23 +1,31 @@
 import { column, defineDb, defineTable } from 'astro:db';
 
+const Courses = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text()
+  }
+})
+
 const Student = defineTable({
   columns: {
-    id: column.number({primaryKey: true}),
+    id: column.number({ primaryKey: true }),
     first_name: column.text(),
     last_name: column.text(),
+    course: column.number({ references: () => Courses.columns.id })
   }
 });
 
 const Note = defineTable({
   columns: {
     body: column.text(),
-    studentId: column.number({references: () => Student.columns.id})
+    studentId: column.number({ references: () => Student.columns.id })
   }
 });
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {Student, Note}
+  tables: { Student, Note, Courses }
 });
 
 // function buxtehude(paramOma, paramOpa) {
--- db/seed.ts
+++ db/seed.ts
@@ -1,19 +1,55 @@
-import { Student, db } from 'astro:db';
+import { Courses, Student, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
+  await db.insert(Courses).values([
+    { title: "FI23-1" },
+    { title: "FI23-2" },
+    { title: "FI23-3" },
+    { title: "FI23-4" },
+    { title: "FI24-1" },
+    { title: "FI24-2" },
+    { title: "FI24-3" },
+    { title: "FI24-4" },
+  ]);
+
+
   await db.insert(Student).values([
     {
       first_name: "Max",
       last_name: "Müller",
+      course: 1
     },
     {
       first_name: "Nadine",
       last_name: "Müller",
+      course: 1
+
     },
     {
       first_name: "Susanne",
       last_name: "Stark",
+      course: 1
+    },
+    {
+      first_name: "Max",
+      last_name: "Müller",
+      course: 2
+    },
+    {
+      first_name: "Nadine",
+      last_name: "Müller",
+      course: 2
+
+    },
+    {
+      first_name: "Susanne",
+      last_name: "Stark",
+      course: 2
     },
   ])
 }
+/**
+ * 
+ * Insert Into Student (first_name, last_name) Values ("Max", "Müller")
+ */
